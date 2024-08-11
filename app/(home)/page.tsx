713724fccf8c95ef51db1bed0e8f008092befb16
Dang <56210432+looks32@users.noreply.github.com>
2024-08-11 20:53:41 +0900

    폴더 위치 수정

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,51 +1,7 @@
-"use client";
+import React from 'react'
 
-import FormButton from "@/components/form-btn";
-import Input from "@/components/input";
-
-import { useFormState } from "react-dom";
-import { logIn, FormState } from "./actions";
-import { PASSWORD_MIN_LENGTH } from "@/lib/constants";
-
-const initialState: FormState = {
-  errors: {},
-  message: "",
-};
-
-export default function Home() {
-
-  const [state, action] = useFormState(logIn, initialState);
-
+export default function Login() {
   return (
-    <div className="flex flex-col gap-10 py-8 px-6">
-      <div className="flex flex-col gap-2 *:font-medium">
-        <h1 className="text-2xl text-center">💙</h1>
-      </div>
-      <form action={action} className="flex flex-col gap-3">
-        <Input
-            name="email"
-            type="email"
-            placeholder="Email"
-            errors={state.errors?.fieldErrors?.email}
-          />
-          <Input
-            name="username"
-            type="text"
-            placeholder="Username"
-            errors={state.errors?.fieldErrors?.username}
-          />
-          <Input
-            name="password"
-            type="password"
-            placeholder="Password"
-            errors={state.errors?.fieldErrors?.password}
-          />
-          <FormButton text="logIn" />
-          {state.message && (
-            <div className="bg-green-600 text-white p-3 rounded-md text-center">Welcome back!</div>
-          )}
-      </form>
-
-    </div>
-  );
+	<div>login</div>
+  )
 }
